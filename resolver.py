14805527c522072103cb5341d1883b2b7db7bde3
estevio <magdalena.paredes@ug.uchile.cl>
2026-09-07 19:31:48 -0300

    fix: actualizar caché + borrado de algunos prints innecesarios

--- resolver.py
+++ resolver.py
@@ -2,10 +2,6 @@ import socket
 import dnslib
 from dnslib import DNSRecord, QTYPE, RR, A
 
-# test 2
-# dig -p8000 @IP_VM example.com
-# dig -p8000 @10.0.2.15 example.com
-
 # Variables
 IP_VM = "10.0.2.15" # magda
 # IP_VM = "192.168.1.18" # matias
@@ -18,19 +14,20 @@ cache = []
 historial = []
 
 def parse_dns_msg(msg):
-    """Parsea un mensaje DNS
+    """Parsea un mensaje DNS.
+    El tipo RR es la estructura RR de dnslib
     Args:
         msg (bin): El mensaje DNS a parsear
 
     Returns:
         dict: Un diccionario con la informacion necesaria
-            - qname: nombre del dominio solicitado
-            - ancount: el numero de respuestas en el mensaje
-            - nscount: numero de registros de servidores en la seccion de autoridad
-            - arcount: numero de registros de servidores en la seccion adicional
-            - answer: las respuestas recibidas
-            - authority: señala los servidores que tienen autoridad
-            - additional: informacion extra, de servidores sin autoridad
+            - qname (str): nombre del dominio solicitado
+            - ancount (int): el numero de respuestas en el mensaje
+            - nscount (int): numero de registros de servidores en la seccion de autoridad
+            - arcount (int): numero de registros de servidores en la seccion adicional
+            - answer (RR): las respuestas recibidas
+            - authority (RR): señala los servidores que tienen autoridad
+            - additional (RR): informacion extra, de servidores sin autoridad
     """
 
     d = DNSRecord.parse(msg)
@@ -47,6 +44,15 @@ def parse_dns_msg(msg):
     return msg_dict
 
 def resolver_a(msg, ip_addr="198.41.0.4"):
+    """Envía un mensaje DNS a la IP especificada y retorna el mensaje DNS recibido
+    Args:
+        msg (bin): La consulta DNS a enviar.
+        ip_addr (str): La dirección IP por consultar. 
+            El valor por default es "198.41.0.4", una IP root.
+
+    Returns:
+        bin: el mensaje de respuesta de la consulta enviada a la dirección IP.
+    """
     server_address = (ip_addr, 53)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # PARTE A
@@ -61,6 +67,9 @@ def resolver_a(msg, ip_addr="198.41.0.4"):
     return data
 
 def resolver_c(msg, response):
+    """Recibe un mensaje DNS y el diccionario que le representa. 
+    Busca respuestas de tipo A o bien NS y retorna la respuesta 
+    """
     if response["arcount"]>0:
         for add in response["additional"]:
             if add.rtype == QTYPE.A:
@@ -96,9 +105,6 @@ def resolver(mensaje_consulta: bytes, ip_addr="198.41.0.4"):
     """"""
     data = resolver_a(mensaje_consulta, ip_addr)
     response = parse_dns_msg(data)
-    #query_limpia = DNSRecord.question(response["qname"]).pack()
-    #data_limpia = resolver_a(query_limpia, ip_addr)
-    #response_limpia = parse_dns_msg(data_limpia)
     # PARTE B
     if (response["ancount"] > 0 and any(r.rtype == QTYPE.A for r in response["answer"])):
         return data
@@ -107,7 +113,6 @@ def resolver(mensaje_consulta: bytes, ip_addr="198.41.0.4"):
     else:
         if debug:
             print("respuesta inmanejable\n")
-            debug_dns_response(data)
 
 def pdebug(msg, ns, ip_ns="198.41.0.4"):
     data_consulta = parse_dns_msg(msg)
@@ -120,7 +125,6 @@ def consultar_en_cache(msg):
     if len(cache) > 0:
         for c in cache:
             if c[0] == dominio:
-                print(c[1])
                 res = DNSRecord.parse(msg).reply()
                 res.add_answer(RR(rname=dominio, rtype=QTYPE.A, rdata=A(c[1])))
                 return res.pack()    
@@ -136,23 +140,27 @@ def actualizar_cache(dominio):
         cnt[dom] = cnt.get(dom, 0) + 1
     top3 = set([t[0] for t in sorted(cnt.items(), key=lambda item: item[1], reverse=True)[:3]])
     # verificamos que el top 3 este en el cache
-    if len(cache) < 3:
+    if len(cache) < 3 and dominio not in [d[0] for d in cache]:
         q = DNSRecord.question(dominio).pack()
         resp = parse_dns_msg(resolver(q))
         for r in resp["answer"]:
             if r.rtype == QTYPE.A:
                 ip_new = str(r.rdata)
-        print(ip_new)
         cache.append((dominio, ip_new))
     else:
         cache_set = set([d[0] for d in cache])
-        dif_cache = (cache_set - top3)
+        dif_cache = cache_set - top3
         # si no, tomamos el distinto y reemplazamos
         if dif_cache:
-            dif_top3 = (top3 - cache_set).pop()
+            dif_cache = dif_cache.pop()
+            dif_top3 = (top3 - cache_set)
+            dif_top3 = dif_top3.pop()
             cache = [t for t in cache if t[0] != dif_cache]
-            ip_new = str(parse_dns_msg(resolver(q))["answer"][0].rdata)
-            #print(ip_new)
+            q = DNSRecord.question(dif_top3).pack()
+            resp = parse_dns_msg(resolver(q))
+            for r in resp["answer"]:
+                if r.rtype == QTYPE.A:
+                    ip_new = str(r.rdata)
             cache.append((dif_top3, ip_new))
     
 if __name__ == "__main__":
@@ -161,19 +169,18 @@ if __name__ == "__main__":
     sock.bind((IP_VM, 8000))
 
     while True:
-        print("while")
+        print("Esperando un nuevo cliente\n")
         msg, client = sock.recvfrom(buff_size)
         cach = consultar_en_cache(msg)
-        print(f"cach:{cach}")
         if cach:
             if(debug):
                 print("Dominio en caché")
             sock.sendto(cach, client)
         else:
-            print(cache)
             if debug:
                 pdebug(msg, ".")
             res = resolver(msg)
             if res:
                 sock.sendto(res, client)
         actualizar_cache(parse_dns_msg(msg)["qname"])
+        print(f"cache actualizado: {cache}\n")
